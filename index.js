a2042ce96e6b216b9c4d04e1101c21aab51dbdc7
JavierMorales <jmoralesr@protonmail.com>
2024-01-17 18:57:20 +0100

    Modificaciones

---
+++ index.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('#botonIniciarSesion').on('click', function () {
+        window.location.href = 'main.html';
+    });
+});
+
+
+$(document).ready(function () {
+    $('#botonInformacion').on('click', function () {
+        window.location.href = 'itinerario.html';
+    });
+});
